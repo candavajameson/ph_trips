98a6b010c6db34a6cf2ee340dda0af1bc5cb8553
Jameson Candava <candavajameson@gmail.com>
2015-02-18 16:46:21 +0800

    add name for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def render_404
   	render file: Rails.root.join("public", "404"), layout: false, status: "404"
   end
@@ -10,4 +12,11 @@ class ApplicationController < ActionController::Base
   def render_403
   	head :forbidden
   end
+
+  protected 
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216041643) do
+ActiveRecord::Schema.define(version: 20150218084024) do
 
   create_table "blogs", force: true do |t|
     t.string   "title"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20150216041643) do
     t.datetime "updated_at"
     t.string   "avatar"
     t.boolean  "admin",                  default: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
