6f39c6518d0feeab4e62d6cddcccbb7a229bb47c
Jameson Candava <candavajameson@gmail.com>
2015-02-19 19:40:12 +0800

    fixed profile-pic header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,17 @@ module ApplicationHelper
 	def truncate_content(content)
 		"#{content[0..150]}..."
 	end
+
+	def avatar_icon(user_email = '', size = nil)
+		user = User.find_by(email: user_email)
+
+		if user
+			user.avatar_url(size) || default_avatar
+		end
+	end
+
+	def default_avatar
+		image_path('no_avatar.png')
+	end
+
 end
